35d4ac64d388e4c0ee00dfe59f1eb7338637b978
prahlad574 <sivaprahlad@gmail.com>
2024-12-21 16:18:37 +0530

    created db context and configured CORS

---
+++ BudgetBackend/Models/Context/BudgetContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BudgetBackend.Models.Context
+{
+    public class BudgetContext : DbContext
+    {
+        public BudgetContext(DbContextOptions<BudgetContext> options): base(options)
+        {
+            
+        }
+    }
+}
