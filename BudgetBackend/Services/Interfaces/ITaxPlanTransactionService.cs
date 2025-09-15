2fa44167ad0c623754c9293181d32e61695694f5
prahlad574 <sivaprahlad@gmail.com>
2025-09-15 22:42:24 +0530

    done chnages for inserting tax plan transactions in backend

--- BudgetBackend/Services/Interfaces/ITaxPlanTransactionService.cs
+++ BudgetBackend/Services/Interfaces/ITaxPlanTransactionService.cs
@@ -1,6 +1,9 @@
-﻿namespace BudgetBackend.Services.Interfaces
+﻿using BudgetBackend.Models.Classes;
+
+namespace BudgetBackend.Services.Interfaces
 {
     public interface ITaxPlanTransactionService
     {
+        public void InsertTaxPlanTransaction(TaxPlanTransactionForFinancialYear taxPlanTransactionDto);
     }
 }
--- BudgetBackend/Services/TaxPlanTransactionService.cs
+++ BudgetBackend/Services/TaxPlanTransactionService.cs
@@ -1,8 +1,26 @@
-﻿using BudgetBackend.Services.Interfaces;
+﻿using BudgetBackend.Models.Classes;
+using BudgetBackend.Models.Entities;
+using BudgetBackend.Repositories.Interfaces;
+using BudgetBackend.Services.Interfaces;
 
 namespace BudgetBackend.Services
 {
     public class TaxPlanTransactionService : ITaxPlanTransactionService
     {
+        private readonly ITaxPlanTransactionRepository _taxPlanTransactionRepository;
+        public TaxPlanTransactionService(ITaxPlanTransactionRepository taxPlanTransactionRepository)
+        {
+            _taxPlanTransactionRepository = taxPlanTransactionRepository;
+        }
+        public void InsertTaxPlanTransaction(TaxPlanTransactionForFinancialYear taxPlanTransactionDto)
+        {
+            _taxPlanTransactionRepository.AddAsync(new TaxPlanTransactionsEntity { 
+                TaxPlanForFinancialYearId = taxPlanTransactionDto.TaxPlanForFinancialYearId,
+                TaxPlanTransactionId = Guid.NewGuid(), // Generating new GUID for the transaction
+                TransactionAmount = taxPlanTransactionDto.TransactionAmount, 
+                TransactionDate = taxPlanTransactionDto.TransactionDate,
+            });
+            _taxPlanTransactionRepository.SaveChanges();
+        }
     }
 }
